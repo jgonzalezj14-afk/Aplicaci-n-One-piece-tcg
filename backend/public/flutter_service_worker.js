de5b282534f87bd015c193297795028e1615404d
Javiglz23 <jaglezcuri@gmail.com>
2026-01-07 14:07:41 +0100

    Versión Final

--- backend/public/flutter_service_worker.js
+++ backend/public/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a501679509e9cc835438435e4f4f451
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9e76e3e7e44c0bcb5727d4e09385f705",
+"flutter_bootstrap.js": "1af5ace7a281ab954809813136d797c7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
